28c2f51cc89a5a15c091ae6f11bc3c00a010a480
Tamar Loach <tamar.loach@spc.ox.ac.uk>
2016-10-31 18:44:49 +0000

    Updated queue length plotting functions

--- Queues_Plots.R
+++ Queues_Plots.R
@@ -4,33 +4,46 @@ library(reshape2)
 
 
 PlotNrPeople <- function(queues){
+  # This function plots the number of people in each queue over time
+  # Args:
+  #     - queues, the result of a call to QueuesFunction
+  # Returns:
+  #     - a ggplot object (can use this as plot arg in ggsave())
   
   queues = queues$queues_list
-  
+  nr_queues = length(queues)
   queues_df <- do.call(rbind, queues)
-  queues_df$nr_queue <- as.factor(rep(1:4, each=nrow(queues[[1]])))
+  queues_df$nr_queue <- as.factor(rep(1:nr_queues, each=nrow(queues[[1]])))
   
-  ggplot(data=queues_df, aes(x=time, y=nr_people))+
-    geom_point()+
-    facet_wrap(~nr_queue)
+  plot = ggplot(data=queues_df, aes(x=time, y=nr_people))+
+                geom_step()+
+                facet_wrap(~nr_queue)
+  return(plot)
 }
 
 
-HistNrPeople <- function(queues, ...){
+HistNrPeople <- function(queues){
+  # This function plots the weighted histogram of the queue legnths for each queue
+  # Args:
+  #     - queues, the result of a call to QueuesFunction
+  # Returns:
+  #     - a ggplot object (can use this as plot arg in ggsave())
   
   queues = queues$queues_list
-  p <- list()
+  new_queues = list()
+  nr_queues = length(queues)
   
   for (i in seq_along(queues)){
     queue <- queues[[i]]
-
     # calculate weights
     queue$time_spent = append((tail(queue, -1) - head(queue, -1))$time, NaN)
-    print(queue)
-    print(summary(queue))
-    p[[i]] <- ggplot(data=queue, aes(queue$nr_people, weight = queue$time_spent)) + 
-      geom_histogram(binwidth = 1) +
-      ggtitle(paste("Queue ", i))
+    new_queues[[i]] = queue
   }
-  do.call(grid.arrange,p)
+  queues_df <- do.call(rbind, new_queues)
+  queues_df$nr_queue <- as.factor(rep(1:nr_queues, each=nrow(new_queues[[1]])))
+  
+  plot = ggplot(data=queues_df, aes(queues_df$nr_people, weight = queues_df$time_spent))+
+                geom_histogram(binwidth = 1) +
+                facet_wrap(~nr_queue)
+  return(plot)
 }
