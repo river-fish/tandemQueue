a279075c5c5b8fbea2c665d2d6a59a40cd066ceb
Tamar Loach <tamar.loach@spc.ox.ac.uk>
2016-10-31 18:03:12 +0000

    Trying to fix multiple plots bug

--- Queues_Plots.R
+++ Queues_Plots.R
@@ -1,31 +1,35 @@
 library(gridExtra)
 library(ggplot2)
+library(reshape2)
 
-#load a simulation of some queues to test plot functions on
-#load('data/test_queues.RData')
 
-PlotNrPeople <- function(queues=test_queues, ...){
-  queues = queues$queues_list
-  p <- list()
+PlotNrPeople <- function(queues){
   
-	for (i in seq_along(queues)){
-		queue <- queues[[i]]
-		p[[i]] <- qplot(queue$time, queue$nr_people, geom="step", direction = "hv", ...)+
-		  ggtitle(paste("Queue ", i))
-	}
-  do.call(grid.arrange,p)
+  queues = queues$queues_list
+  
+  queues_df <- do.call(rbind, queues)
+  queues_df$nr_queue <- as.factor(rep(1:4, each=nrow(queues[[1]])))
+  
+  ggplot(data=queues_df, aes(x=time, y=nr_people))+
+    geom_point()+
+    facet_wrap(~nr_queue)
 }
 
 
-HistNrPeople <- function(queues=test_queues, ...){
+HistNrPeople <- function(queues, ...){
   
   queues = queues$queues_list
   p <- list()
   
   for (i in seq_along(queues)){
     queue <- queues[[i]]
-    p[[i]] <- ggplot(data=queue, aes(queue$nr_people)) + 
-      geom_histogram(binwidth = 1000/max(queue$nr_people)) +
+
+    # calculate weights
+    queue$time_spent = append((tail(queue, -1) - head(queue, -1))$time, NaN)
+    print(queue)
+    print(summary(queue))
+    p[[i]] <- ggplot(data=queue, aes(queue$nr_people, weight = queue$time_spent)) + 
+      geom_histogram(binwidth = 1) +
       ggtitle(paste("Queue ", i))
   }
   do.call(grid.arrange,p)
