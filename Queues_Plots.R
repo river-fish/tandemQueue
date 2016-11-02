b403245430c7b7bfd33cc02e1c0052cd471fd42f
river-fish <river-fish@users.noreply.github.com>
2016-11-02 14:57:28 +0000

    Fixed problem with bar widths
    lower x lim now forced to zero. Upper is data driven

--- Queues_Plots.R
+++ Queues_Plots.R
@@ -25,13 +25,14 @@ PlotNrPeople <- function(queues){
   
   plot = ggplot(data=queues_df, aes(x=time, y=nr_people))+
                 geom_step()+
+                labs(x = "t", y = "State (queue length)", size=16)+
                 facet_wrap(~nr_queue)
   return(plot)
 }
 
 
 
-HistNrPeopleWithGeom <- function(queues, rho){
+HistNrPeopleWithGeom <- function(queues, rho, ...){
   # This function plots the normalised weighted histogram of the queue lengths for each queue
   # Args:
   #     - queues, the first element of the result of a call to QueuesFunction
@@ -45,34 +46,28 @@ HistNrPeopleWithGeom <- function(queues, rho){
     queue <- queues[[i]]
     # calculate weights
     queue$time_spent = append((tail(queue, -1) - head(queue, -1))$time, NaN)
-    #queue_total_time = sum(queue$time_spent, na.rm=TRUE)
-    #queue$time_spent = queue$time_spent / queue_total_time
     queue$nr_queue <- rep(i,nrow(queue))
     new_queues[[i]] = queue
     
   }
+  
   queues_df <- do.call(rbind, new_queues)
   
   queues_df <- queues_df[, c('nr_queue', 'nr_people', 'time_spent')]
-  print(head(queues_df))
   
-  geom_df = as.data.frame(lapply(1:nr_queues, function(i) rgeom(10000, rho[i])))
+  geom_df = as.data.frame(lapply(1:nr_queues, function(i) rgeom(100000, rho[i])))
   colnames(geom_df) = 1:nr_queues
   geom_df = melt(geom_df)
   colnames(geom_df) = c('nr_queue', 'nr_people')
-  geom_df$time_spent = rep(1, nr_queues)
-  print(head(geom_df))
-  
-  queues_df$label <- "Queue"
-  geom_df$label <- "Geometric"
-  plot_df = rbind(geom_df, queues_df)
-  
-  plot = ggplot()+
-    geom_histogram(data=queues_df, aes(x=nr_people, y=..count../sum(..count..), fill='Geometric', weight=time_spent), binwidth = 1, alpha = 0.3, colour='black')+
-    geom_histogram(data=geom_df, aes(x=nr_people, y=..count../sum(..count..), fill='Queue', weight=time_spent), binwidth = 1, alpha = 0.3, colour='black')+
-    labs(x = "State (queue length)", y = "Proportion of time spent in state")+
-    facet_wrap(~nr_queue)
+
+   plot = ggplot()+
+     geom_histogram(data=queues_df, aes(x=nr_people, y=1*..density.., fill='Queue', weight=time_spent), alpha =0.8, binwidth=1, ...)+
+     geom_histogram(data=geom_df, aes(x=nr_people, y=1*..density.., fill='Geometric', show_guide=FALSE), alpha = 0, colour='#FF9999',show_guide=FALSE, binwidth=1,  ...)+
+     labs(x = "State (queue length)", y = "Proportion of time spent in state", size=16)+
+     guides(fill=guide_legend(title=NULL)) +
+     theme(legend.key = element_blank()) +
+     expand_limits(x=0)
+     facet_wrap(~nr_queue)
 
   return(plot)
 }
-
